54cf753914a51ea245a83f3177f160bb81118719
BNSBNS25 <39655987+BNSBNS25@users.noreply.github.com>
2019-09-10 13:53:29 +0300

    1

--- src/index.js
+++ src/index.js
@@ -13,8 +13,12 @@ board.addEventListener("mousedown", function(ev) {
   let td = ev.target;
   if (player === 1) {
     td.innerHTML = "x";
+    td.classList.add("player1");
+    td.classList.remove("player2");
   } else {
     td.innerHTML = "o";
+    td.classList.add("player2");
+    td.classList.remove("player1");
   }
   player = (player + 1) % 2;
 });
