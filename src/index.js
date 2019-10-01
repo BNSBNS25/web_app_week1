5f5f4c61c7235ebd9b4c0967c3436c5a2bb6d72f
BNSBNS25 <39655987+BNSBNS25@users.noreply.github.com>
2019-10-01 14:00:07 +0300

    [Add] Materialize

--- src/index.js
+++ src/index.js
@@ -1,24 +1,44 @@
-let board = document.getElementById("board").firstElementChild;
-for (let i = 0; i < 5; i++) {
-  let tr = document.createElement("tr");
-  for (let j = 0; j < 5; j++) {
-    let td = document.createElement("td");
+let board = document
+  .getElementById("board")
+  .getElementsByClassName("game-field")[0];
 
-    tr.append(td);
-  }
-  board.append(tr);
-}
+let progress = document
+  .getElementById("board")
+  .getElementsByClassName("determinate")[0];
+let perc = 0;
 let player = 1;
+window.setInterval(function() {
+  if (perc === 100) {
+    perc = 0;
+    player = (player + 1) % 2;
+  }
+  progress.style.width = perc + "%";
+  perc++;
+}, 100);
+
+for (let i = 0; i < 5; i++) {
+  let row = document.createElement("div");
+  row.classList.add("row");
+  for (let j = 0; j < 5; j++) {
+    let col = document.createElement("div");
+    col.classList.add("col");
+    col.classList.add("s1");
+    row.append(col);
+  }
+  board.append(row);
+}
+
 board.addEventListener("mousedown", function(ev) {
-  let td = ev.target;
+  let div = ev.target;
+  perc = 0;
   if (player === 1) {
-    td.innerHTML = "x";
-    td.classList.add("player1");
-    td.classList.remove("player2");
+    div.innerHTML = "x";
+    div.classList.add("player1");
+    div.classList.remove("player2");
   } else {
-    td.innerHTML = "o";
-    td.classList.add("player2");
-    td.classList.remove("player1");
+    div.innerHTML = "o";
+    div.classList.add("player2");
+    div.classList.remove("player1");
   }
   player = (player + 1) % 2;
 });
